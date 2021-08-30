53001f74bac6c4f490b7239d3cf3921f4e510246
Carlos Castro Brenes <ccasbre27@hotmail.com>
2021-08-30 11:35:33 -0600

    add basic components

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom";
+import { useState } from "react";
+
+const App = () => {
+
+    const [input, setInput] = useState('');
+    const [code, setcode] = useState('');
+
+    const handlerOnClick = () => {
+        console.log(input);
+    };
+
+    return <div>
+        <textarea value={input} onChange={e => setInput(e.target.value)}>
+        </textarea>
+        <div>
+            <button onClick={handlerOnClick}>Submit</button>
+            <pre>{code}</pre>
+        </div>
+    </div>;
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+    document.querySelector('#root')
+)
